f4b8366ed3e3d57b61b2edee54f9d7cd8c5470e0
Jandri Jose Gaona Gaona Macbook <>
2021-12-23 23:20:51 +0100

    definita la sezione TESTIMONIALS

--- assets/js/main.js
+++ assets/js/main.js
@@ -93,7 +93,7 @@ modalCloses.forEach(modalClose => {
 })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-let swiper = new Swiper('.portfolio__container', {
+let swiperPortfolio = new Swiper('.portfolio__container', {
     cssMode: true,
     loop: true,
     navigation: {
@@ -104,12 +104,24 @@ let swiper = new Swiper('.portfolio__container', {
         el: '.swiper-pagination',
         clickable: true,
     },
-    mousewheel: true,
-    keyboard: true,
 });
 
 /*==================== TESTIMONIAL ====================*/
-
+let swiperTestimonial = new Swiper('.testimonial__container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        }
+    }
+});
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 
